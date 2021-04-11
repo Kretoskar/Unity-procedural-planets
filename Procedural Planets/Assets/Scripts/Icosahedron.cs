9ff1e5eb3d642abebd73774fdd8d3f0f62aa5417
Oskar <kogutoskar123@gmail.com>
2021-04-11 19:47:29 +0200

    Fix wrong vertices. Add rendering icosahedron

--- Procedural Planets/Assets/Scripts/Icosahedron.cs
+++ Procedural Planets/Assets/Scripts/Icosahedron.cs
@@ -3,62 +3,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Icosahedron
+public class Icosahedron : MeshData
 {
-    private List<Vector3> _vertices;
-    private List<Vector3> _faces;
+    public override List<Vector3> Vertices() => GetVertices();
 
-    public Icosahedron()
+    public override List<int> Triangles() => GetTriangles();
+
+    private List<Vector3> GetVertices()
     {
-        _vertices = new List<Vector3>();
-        _faces = new List<Vector3>();
-    }
-    
-    private void GetVertices()
-    {
+        List<Vector3> vertices = new List<Vector3>();
+        
         float t = (1 + Mathf.Sqrt(5)) / 2;
      
-        _vertices.Add(new Vector3(-1, t, 0));
-        _vertices.Add(new Vector3(1, t, 0));
-        _vertices.Add(new Vector3(-1, -t, 0));
-        _vertices.Add(new Vector3(-1, -t, 0));
+        vertices.Add(new Vector3(-1, t, 0));
+        vertices.Add(new Vector3(1, t, 0));
+        vertices.Add(new Vector3(-1, -t, 0));
+        vertices.Add(new Vector3(1, -t, 0));
         
-        _vertices.Add(new Vector3(0, -1, t));
-        _vertices.Add(new Vector3(0, 1, t));
-        _vertices.Add(new Vector3(0, -1, -t));
-        _vertices.Add(new Vector3(0, 1, -t));
+        vertices.Add(new Vector3(0, -1, t));
+        vertices.Add(new Vector3(0, 1, t));
+        vertices.Add(new Vector3(0, -1, -t));
+        vertices.Add(new Vector3(0, 1, -t));
         
-        _vertices.Add(new Vector3(t, 0, -t));
-        _vertices.Add(new Vector3(t, 0, 1));
-        _vertices.Add(new Vector3(-t, 0, -1));
-        _vertices.Add(new Vector3(-t, 1, 1));
+        vertices.Add(new Vector3(t, 0, -1));
+        vertices.Add(new Vector3(t, 0, 1));
+        vertices.Add(new Vector3(-t, 0, -1));
+        vertices.Add(new Vector3(-t, 0, 1));
+
+        return vertices;
     }
 
-    private void GetFaces()
+    private List<int> GetTriangles()
     {
-        //given parameters are indexes of vertices on _vertices list
-        _faces.Add(new Vector3(0,11,5));
-        _faces.Add(new Vector3(0,5,1));
-        _faces.Add(new Vector3(0,1,7));
-        _faces.Add(new Vector3(0,7,10));
-        _faces.Add(new Vector3(0,10,11));
-        
-        _faces.Add(new Vector3(1,5,9));
-        _faces.Add(new Vector3(5,11,4));
-        _faces.Add(new Vector3(11,10,2));
-        _faces.Add(new Vector3(10,7,6));
-        _faces.Add(new Vector3(7,1,8));
-        
-        _faces.Add(new Vector3(3,9,4));
-        _faces.Add(new Vector3(3,4,2));
-        _faces.Add(new Vector3(3,2,6));
-        _faces.Add(new Vector3(3,6,8));
-        _faces.Add(new Vector3(3,8,9));
-        
-        _faces.Add(new Vector3(4,9,5));
-        _faces.Add(new Vector3(2,4,11));
-        _faces.Add(new Vector3(6,2,10));
-        _faces.Add(new Vector3(8,6,7));
-        _faces.Add(new Vector3(9,8,1));
+        return new List<int>()
+        {
+            0, 11, 5,
+            0, 5, 1,
+            0, 1, 7,
+            0, 7, 10,
+            0, 10, 11,
+
+            1, 5, 9,
+            5, 11, 4,
+            11, 10, 2,
+            10, 7, 6,
+            7, 1, 8,
+
+            3, 9, 4,
+            3, 4, 2,
+            3, 2, 6,
+            3, 6, 8,
+            3, 8, 9,
+
+            4, 9, 5,
+            2, 4, 11,
+            6, 2, 10,
+            8, 6, 7,
+            9, 8, 1
+        };
     }
 }
